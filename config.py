f7d931aefca717982db3e8471ac61200f94b4daa
EpocDotFr <contact@epoc.fr>
2024-03-26 17:58:32 +0100

    Page LAN [no ci]

--- config.py
+++ config.py
@@ -15,6 +15,7 @@ CONFIG = {
     'assets_bundles': [
         ('css_base', ('css/base.css',), {'filters': 'cssutils', 'output': 'css/base.min.css'}),
         ('css_index', ('css/base.css', 'css/index.css'), {'filters': 'cssutils', 'output': 'css/index.min.css'}),
+        ('css_lan', ('css/base.css', 'css/lan.css'), {'filters': 'cssutils', 'output': 'css/lan.min.css'}),
     ],
     'contexts': [
         (r'index\.html', {
