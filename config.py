f397bbcb756ef6ef388c9233c47617c8488e77e1
EpocDotFr <contact@epoc.fr>
2024-03-30 14:20:17 +0100

    Page LAN

--- config.py
+++ config.py
@@ -10,7 +10,6 @@ STATIC_DIRECTORIES_TO_COPY = [
 
 ASSETS_BUNDLES = [
     ('css_base', ('css/base.css',), {'filters': 'cssutils', 'output': 'css/base.min.css'}),
-    ('css_index', ('css/base.css', 'css/index.css'), {'filters': 'cssutils', 'output': 'css/index.min.css'}),
     ('css_lan', ('css/base.css', 'css/lan.css'), {'filters': 'cssutils', 'output': 'css/lan.min.css'}),
 ]
 
