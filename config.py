83819a11302aeff1646f550ec007c7e06a4c1b82
EpocDotFr <contact@epoc.fr>
2024-04-11 14:49:27 +0200

    Suppression de valeurs de configuration obsolètes

--- config.py
+++ config.py
@@ -2,14 +2,6 @@ from datetime import date
 
 LOCALE = ['fr-FR', 'fr_FR', 'fr_FR.UTF8', 'fr_FR.UTF-8']
 
-STATIC_FILES_TO_COPY = [
-    'favicon.ico',
-]
-
-STATIC_DIRECTORIES_TO_COPY = [
-    'images',
-]
-
 ASSETS_BUNDLES = [
     ('css_base', ('css/base.css',), {'filters': 'cssutils', 'output': 'css/base.min.css'}),
     ('css_lan', ('css/base.css', 'css/lan.css'), {'filters': 'cssutils', 'output': 'css/lan.min.css'}),
