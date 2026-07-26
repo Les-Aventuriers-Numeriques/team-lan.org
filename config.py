7afee5904fe60bf884f3a0f818271be8cbc4a988
Maxime Gross <EpocDotFr@users.noreply.github.com>
2026-07-26 23:10:10 +0200

    MàJ participants LAN

--- config.py
+++ config.py
@@ -55,7 +55,7 @@ CONTEXTS = [
         ],
     }),
     (r'lan\.html', {
-        'current_attendees': 7,
+        'current_attendees': 8,
         'max_attendees': 14,
         'start_date': date(2026, 10, 17),
         'end_date': date(2026, 10, 21),
