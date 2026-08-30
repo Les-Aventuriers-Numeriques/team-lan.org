891629fc4bf39528c5f5fc43e3db8ee18fc7c896
EpocDotFr <contact@epoc.fr>
2026-08-30 15:27:23 +0200

    Passage complet à bootstrap-icons

---
+++ scripts/copy.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -e # Fait en sorte que les prochaines commandes en échec font sortir du script immédiatement
+
+cp node_modules/bootstrap-icons/font/fonts static/fonts/bootstrap-icons -r
